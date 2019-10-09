8ea6e7dcce1eec0dfb305dea066713db5efd689d
fcarlone <frankcarlone@gmail.com>
2019-10-09 14:15:49 -0400

    Add README.md file

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 
+// Passport packages
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect database
+
+// Middleware
+
+// Passport middleware
+
+// Serve static assets
+
+// Define routes
+
+// Route to load single HTML page
 
 // Test route (DELETE after front-end build)
 app.get("/", (req, res) => {
