0280f724b027ba6672eb1317339a64979436d5a2
fcarlone <frankcarlone@gmail.com>
2019-10-18 15:54:56 -0400

    Add Owner Model and config passport.js file

---
+++ models/owner.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Owner = sequelize.define("Owner", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return Owner;
+};
